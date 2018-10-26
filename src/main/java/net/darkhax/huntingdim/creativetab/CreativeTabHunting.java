3c3046e1a1c8d0c0676cf291e5069f02833eeb09
Tyler Hancock <darklime@live.ca>
2018-10-26 03:33:13 -0600

    Added event items to the creative tab while event is active.

--- src/main/java/net/darkhax/huntingdim/creativetab/CreativeTabHunting.java
+++ src/main/java/net/darkhax/huntingdim/creativetab/CreativeTabHunting.java
@@ -1,8 +1,10 @@
 package net.darkhax.huntingdim.creativetab;
 
 import net.darkhax.huntingdim.HuntingDimension;
+import net.darkhax.huntingdim.events.EventLoader;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 
 public class CreativeTabHunting extends CreativeTabs {
 
@@ -15,5 +17,24 @@ public class CreativeTabHunting extends CreativeTabs {
     public ItemStack createIcon () {
 
         return new ItemStack(HuntingDimension.frame);
+    }
+    
+    @Override
+    public void displayAllRelevantItems (NonNullList<ItemStack> itemList) {
+        
+        super.displayAllRelevantItems(itemList);
+        
+        if (EventLoader.currentEvent != null) {
+            
+            for (ItemStack stack : EventLoader.currentEvent.getHeldItems()) {
+                
+                itemList.add(stack);
+            }
+            
+            for (ItemStack stack : EventLoader.currentEvent.getWornItems()) {
+                
+                itemList.add(stack);
+            }
+        }
     }
 }
